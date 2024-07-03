e20385813230a531a71fe001004658aa5c5270e2
Ambinintsoa <118436627+Ambinintsoa@users.noreply.github.com>
2024-07-03 18:03:42 +0300

    statistic page

--- lib/pages/home/widgets/statistics_page.dart
+++ lib/pages/home/widgets/statistics_page.dart
@@ -1,14 +1,57 @@
 import 'package:flutter/material.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:gardenapp/pages/home/widgets/expense_graph.dart';
+import 'package:gardenapp/pages/home/widgets/expense_income.dart';
+import 'package:gardenapp/pages/home/widgets/progress_chart.dart';
 
 class StatisticsPage extends StatelessWidget {
   const StatisticsPage({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final screenHeight = MediaQuery.of(context).size.height;
     return Scaffold(
       appBar: AppBar(
-        title:const Text('Statistics'),
+        title: const Text('Statistics'),
+      ),
+      backgroundColor: Colors.white,
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 10),
+          child: Column(
+            children: [
+              Row(
+                children: const [
+                  ExpenseIncomeData(),
+                ],
+              ),
+              const SizedBox(height: 8),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 22),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const Text(
+                      "Analytics",
+                      style: TextStyle(
+                        fontSize: 17,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    SizedBox(
+                      height:300, // 40% of screen height
+                      child: const ExpenseGraphDesign(),
+                    ),
+                    SizedBox(
+                      height: 120, // 20% of screen height
+                      child: const CircleProgressChart(),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
